74219418faf610f2e1ff757239c4c8239c5bce59
KazenWilver <starkeliude90@gmail.com>
2026-05-18 20:34:12 +0100

    A recomeçar para melhorar

--- frontend/src/app/core/jwt.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('sgfp_token');
-  if (token) {
-    req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
-  }
-  return next(req);
-};
